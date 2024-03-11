cd6a3afb259c0825c08ffbb4e6aa985e73729e7e
dunbar-brett <dunbar.brett@gmail.com>
2024-03-11 14:59:06 -0700

    removing comments from contact form

--- src/ContactForm.js
+++ src/ContactForm.js
@@ -4,37 +4,6 @@ export default function ContactForm({saveContact}) {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [address, setAddress] = useState('');
-  // const [message, setMessage] = useState('');
-
-  // const handleSubmit = async (e) => {
-  //   e.preventDefault();
-
-  //   try {
-  //     let res = await fetch('/api/contacts', {
-  //       method: 'POST',
-  //       body: JSON.stringify({
-  //         name: name,
-  //         email: email,
-  //         address: address,
-  //       }),
-  //       headers: {
-  //         'Content-Type': 'application/json'
-  //       }
-  //     });
-
-  //     let resJson = await res.json();
-
-  //     if (res.status === 200) {
-  //       setName('');
-  //       setEmail('');
-  //       setMessage('User created successfully');
-  //     } else {
-  //       setMessage(`Some error occured: ${resJson.message}`);
-  //     }
-  //   } catch (err) {
-  //     console.log(err);
-  //   }
-  // };
 
   return (
     <div>
