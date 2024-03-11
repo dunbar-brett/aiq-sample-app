b742c0cc31e2a59b1fc379c8fc7d4ce2ab8f2cd3
dunbar-brett <dunbar.brett@gmail.com>
2024-03-11 15:59:01 -0700

    added tailwind and styles to components

--- src/ContactForm.js
+++ src/ContactForm.js
@@ -1,50 +1,59 @@
 import { useState } from 'react';
 
+const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+const validateEmail = (email) => emailRegex.test(email);
+
 export default function ContactForm({saveContact}) {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [address, setAddress] = useState('');
-  // const [message, setMessage] = useState('');
+  const [message, setMessage] = useState('');
 
-  // const handleSubmit = async (e) => {
-  //   e.preventDefault();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setMessage('');
+    if (!name) {
+      setMessage('Name is required');
+      return;
+    }
 
-  //   try {
-  //     let res = await fetch('/api/contacts', {
-  //       method: 'POST',
-  //       body: JSON.stringify({
-  //         name: name,
-  //         email: email,
-  //         address: address,
-  //       }),
-  //       headers: {
-  //         'Content-Type': 'application/json'
-  //       }
-  //     });
+    if (!email) {
+      setMessage('Email is required');
+      return;
+    }
 
-  //     let resJson = await res.json();
+    if (!validateEmail(email)) {
+      setMessage('Email is invalid');
+      return;
+    }
 
-  //     if (res.status === 200) {
-  //       setName('');
-  //       setEmail('');
-  //       setMessage('User created successfully');
-  //     } else {
-  //       setMessage(`Some error occured: ${resJson.message}`);
-  //     }
-  //   } catch (err) {
-  //     console.log(err);
-  //   }
-  // };
+    saveContact(e, {name, email, address});
+  }
 
   return (
-    <div>
-      <h1>Contact Form</h1>
-      <form onSubmit={(e) => saveContact(e, {name: name, email: email, address: address})}>
-        <input type='text' placeholder='Name' onChange={(e) => setName(e.target.value)} />
-        <input type='email' placeholder='Email' onChange={(e) => setEmail(e.target.value)} />
-        <input type='text' placeholder='Address' onChange={(e) => setAddress(e.target.value)} />
-        <button>Submit</button>
+    <section className='w-96 bg-gray-200 shadow-md rounded px-8 pt-6 pb-8 mb-4 h-1/2 mt-11'>
+      <h3 className="text-center font-bold text-xl pb-4">Contact Form</h3>
+      <form className='space-y-3 mx-auto' onSubmit={handleSubmit}>
+        <input type='text' 
+          placeholder='Name'
+          className='peer block w-full rounded-md border border-gray-300 py-2 pl-2'
+          onChange={(e) => setName(e.target.value)}
+        />
+        
+        <input type='email'
+          placeholder='Email'
+          className='peer block w-full rounded-md border border-gray-300 py-2 pl-2'
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <input type='text'
+          placeholder='Address'
+          className='peer block w-full rounded-md border border-gray-300 py-2 pl-2'
+          onChange={(e) => setAddress(e.target.value)}
+        />
+        <button className='w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2  focus:outline-none'>Submit</button>
       </form>
-    </div>
+      <p className='text-red-600 pt-4 text-center'>{message}</p>
+    </section>
   );
 }
--- src/Contacts.js
+++ src/Contacts.js
@@ -1,16 +1,16 @@
 export default function Contacts({contacts}) {
   return (
-    <>
-    <h2>Contacts:</h2>
-    {
-      contacts.map((contact, index) => (
-        <div key={index}>
-          <h3>{contact.name}</h3>
-          <h4>{contact.email}</h4>
-          <p>{contact.address}</p>
-        </div>
-      ))
-    }
-    </>
+    <aside className='w-1/2 px-4'>
+      <h2 className="font-bold text-xl pb-4">Contacts:</h2>
+      {
+        contacts.map((contact, index) => (
+          <div key={index} className='w-96 bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4'>
+            <p><strong>Name:</strong> {contact.name}</p>
+            <p><strong>Email:</strong> {contact.email}</p>
+            <p><strong>Address:</strong> {contact.address}</p>
+          </div>
+        ))
+      }
+    </aside>
   );
 }
