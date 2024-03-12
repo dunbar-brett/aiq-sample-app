4e629d436edb58e50b8420bd298926f6d613db37
dunbar-brett <dunbar.brett@gmail.com>
2024-03-11 23:30:12 -0700

    fixed component order on small display

--- src/Contacts.js
+++ src/Contacts.js
@@ -1,6 +1,6 @@
 export default function Contacts({contacts}) {
   return (
-    <aside className='px-4 h-fit'>
+    <aside className='md:row-start-1 row-start-2 px-4 h-fit'>
       <h2 className="font-bold text-xl pb-4">Contacts:</h2>
       {
         contacts.map((contact, index) => (
